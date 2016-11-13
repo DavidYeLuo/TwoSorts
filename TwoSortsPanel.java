9ac2d1be87fc2ae2f41e5ee273d70b6b8d4a4f99
DavidYeLuo <dayeluo@galileoweb.org>
2016-11-12 22:01:52 -0800

    Added Selection Sort

--- TwoSortsPanel.java
+++ TwoSortsPanel.java
@@ -16,15 +16,15 @@ public class TwoSortsPanel extends JPanel
 		step = 0;
 		
 		selectionNumbers = new Integer[10];
-		selectionNumbers[0] = 2;
+		selectionNumbers[0] = 10;
 		selectionNumbers[1] = 5;
 		selectionNumbers[2] = 1;
 		selectionNumbers[3] = 3;
 		selectionNumbers[4] = 4;
 		selectionNumbers[5] = 7;
-		selectionNumbers[6] = 6;
+		selectionNumbers[6] = 2;
 		selectionNumbers[7] = 9;
-		selectionNumbers[8] = 10;
+		selectionNumbers[8] = 6;
 		selectionNumbers[9] = 8;
 		
 		insertionNumbers = selectionNumbers.clone();
@@ -41,60 +41,73 @@ public class TwoSortsPanel extends JPanel
 	{
 		super.paintComponent(g);
 
-		final int width = 10;
-		final int middle = 250;
-		final int gap = 20;
+		final int width = 10;  // width of the bars
+		final int center = 250;  // location at half of the screen
+		final int gap = 20;  // gap between bars
 		
-		
+		// selection sort
 		int start = 150;
 		g.setColor(Color.white);
-		g.drawString("Selection Sort", middle/2+80, 60);
+		g.drawString("Selection Sort", center/2+80, 60);
 		for(int i=0; i<selectionNumbers.length; i++)
 		{
 			g.setColor(Color.red);
-			g.fillRect(start, middle/2+50 - selectionNumbers[i]*10, width, selectionNumbers[i]* 10);
+			g.fillRect(start, center/2+50 - selectionNumbers[i]*10, width, selectionNumbers[i]* 10);
 			g.setColor(Color.white);
-			g.drawString(selectionNumbers[i].toString(), start, middle/2+50 +20);
+			g.drawString(selectionNumbers[i].toString(), start, center/2+50 +20);
 			start += gap;
 		}
 		
+		// insertion sort
 		start = 150; //reset gap
 		g.setColor(Color.white);
-		g.drawString("Insertion Sort", middle/2+80, 240);
+		g.drawString("Insertion Sort", center/2+80, 240);
 		for(int i=0; i<insertionNumbers.length;i++)
 		{
 			g.setColor(Color.green);
-			g.fillRect(start, middle+100 - insertionNumbers[i]*10, width, insertionNumbers[i]* 10);
+			g.fillRect(start, center+100 - insertionNumbers[i]*10, width, insertionNumbers[i]* 10);
 			g.setColor(Color.white);
-			g.drawString(insertionNumbers[i].toString(), start, middle+100 +20);
+			g.drawString(insertionNumbers[i].toString(), start, center+100 +20);
 			start += 20;
 		}
 		
+		// show the step
 		g.setColor(Color.white);
-		g.drawString("Step: " + step, middle/2 -50, middle-30);
+		g.drawString("Step: " + step, center/2 -50, center-30);
 		
 	}
 	
 	public void selectionSort()
 	{
-//		int smallest = selectionNumbers[0];
-//		for(int y=0; y<selectionNumbers.length;y++)
-//		{
-//			for(int x=0; x<selectionNumbers.length;x++)
-//			{
-//				if(selectionNumbers)
-//			}
-//		}
+		if(step < selectionNumbers.length)
+		{
+			int min = step;
+			int temp = selectionNumbers[step];
+			
+			for(int scan=step+1; scan<selectionNumbers.length;scan++)
+			{
+				if(selectionNumbers[scan].compareTo(selectionNumbers[min]) < 0)
+				{
+					temp = selectionNumbers[scan];
+					min = scan;
+				}
+			}
+			
+			//swap the values
+			selectionNumbers[min] = selectionNumbers[step];
+			selectionNumbers[step] = temp;
+		}
 	}
 	public void insertionSort()
 	{
-		
 	}
 	private class NextButton implements ActionListener
 	{
 
 		public void actionPerformed(ActionEvent e) 
 		{
+			selectionSort();
+			insertionSort();
 			step++;
 			repaint();
 		}
